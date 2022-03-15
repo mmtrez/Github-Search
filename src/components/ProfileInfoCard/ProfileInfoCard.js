277e213b3960356019e2105e92a9bc80a86bbf21
mmtrez <immtrez@gmail.com>
2022-03-15 12:33:00 +0330

    fix p

--- src/components/ProfileInfoCard/ProfileInfoCard.js
+++ src/components/ProfileInfoCard/ProfileInfoCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ProfileInfoCard = ({
-  userInfo: { avatar_url, name, location, bio, html_url, login },
+  userInfo: { avatar_url, name, location, bio, html_url, login, company, blog },
 }) => {
   return (
     <div className="info-section-container card">
@@ -13,14 +13,15 @@ const ProfileInfoCard = ({
       <div className="more-info">
         <>
           <h2>Bio :</h2>
-          <p>{bio ?? "This user didn't provide any bio!"}</p>
+          <p className="p-bio">{bio ?? "This user didn't provide any bio!"}</p>
         </>
         <a href={html_url}>
           <button className="btn btn-dark my-1">Visit Github Page</button>
         </a>
-        <div className="flex">
-          <p>Login : </p>
-          <p>{login}</p>
+        <div>
+          <p>Login : {login}</p>
+          {company !== null && <p>Company : {company}</p>}
+          {blog !== null && <p>Blog : {blog}</p>}
         </div>
       </div>
     </div>
